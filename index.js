ad4cdcc5fb15b138afe9a2737dee0838ed5a06bc
Pedro157240 <peedrohrk@gmail.com>
2026-04-09 09:06:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ const player4 = {
     return Math.floor(Math.random() * 6) + 1;
 }
 
+async function playRaceEngine(character1, character2) {
+    for(let round = 1; round < 6; round++) {
+        console.log(`Turno ${round}`);
+
+        // Sortear ação
+    }
+}
+
 (async function main() {
     console.log(`Uma batalha entre ${player1.Nome} e ${player2.Nome} foi iniciada... \n`);
+await playRaceEngine(player1, player2);
 })();
